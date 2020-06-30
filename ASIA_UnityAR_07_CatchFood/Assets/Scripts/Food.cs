714a620370730f862e715b3918ae91ea5c064b9e
morgan88888 <55180693+morgan88888@users.noreply.github.com>
2020-07-01 05:38:29 +0800

    完成第一版

---
+++ ASIA_UnityAR_07_CatchFood/Assets/Scripts/Food.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class Food : MonoBehaviour
+{
+    [Header("爆")]
+    public GameObject explosion;
+
+    private NavMeshAgent nma;
+
+    private void Start()
+    {
+        nma = GetComponent<NavMeshAgent>();
+    }
+
+    private void CreateEffect()
+    {
+        GameObject expl = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(expl, 0.5f);
+        Destroy(gameObject);
+    }
+
+
+
+    private void ClickAndDead()
+    {
+        CreateEffect();
+    }
+    
+    private void OnMouseDown()
+    {
+        ClickAndDead();
+    }
+
+}
---
+++ ASIA_UnityAR_07_CatchFood/Assets/Scripts/Spawn.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Spawn : MonoBehaviour
+{
+    private static Transform target;
+
+    private int count;
+
+    public void Replay()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
